753d202b09440087cf5e29451aa96d9e9c1c0067
Muhammad Rizwan <xrizwanr@gmail.com>
2022-08-13 21:33:28 +0500

    first commit

---
+++ lib/messengyr/accounts/accounts.ex
@@ -0,0 +1,16 @@
+defmodule Messengyr.Accounts do
+  import Ecto.Changeset
+  alias Messengyr.Accounts.User
+  alias Messengyr.Repo
+
+  def create_user(params) do
+    register_changeset(params)
+      |> Repo.insert
+  end
+
+  def register_changeset(params \\ %{}) do
+    %User{}
+      |> cast(params, [:username, :email, :password])
+      |> validate_required([:username, :email, :password])
+  end
+end
---
+++ lib/messengyr_web/controllers/page_controller.ex
@@ -0,0 +1,34 @@
+defmodule MessengyrWeb.PageController do
+  use MessengyrWeb, :controller
+  alias Messengyr.Accounts;
+
+  def index(conn, _params) do
+    render conn
+  end
+
+  def login(conn, _params) do
+    render conn
+  end
+
+  def signup(conn, _params) do
+    changeset = Accounts.register_changeset()
+
+    render conn, user_changeset: changeset
+  end
+
+  def create_user(conn, %{"user" => user_params}) do
+    IO.puts("Create User!")
+    IO.inspect(user_params)
+
+    case Accounts.create_user(user_params) do
+      {:ok, _user} ->
+        conn
+          |> put_flash(:info, "User created successfully!")
+          |> redirect(to: "/")
+      {:error, user_changeset} ->
+        conn
+          |> put_flash(:error, "Unable to create account!")
+          |> render("signup.html", user_changeset: user_changeset)
+    end
+  end
+end
