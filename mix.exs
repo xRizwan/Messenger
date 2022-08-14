765a68714d12986b299aaed20f77b67b1d78ba50
Muhammad Rizwan <xrizwanr@gmail.com>
2022-08-15 03:56:43 +0500

    removed unncessary node_modules

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule Messengyr.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:dart_sass, "~> 0.5", runtime: Mix.env() == :dev}
+      {:dart_sass, "~> 0.5", runtime: Mix.env() == :dev},
     ]
   end
 
